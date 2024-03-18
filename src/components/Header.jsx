2d8b1034d281d591a971de352a16227c9ad49126
henryistyping <mykim1891@gmail.com>
2024-03-18 13:17:07 -0700

    header created + populated it with list from index

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import { brainwave } from "../assets"
+import { navigation } from "../constants"
 
 const Header = () => {
   return (
@@ -10,7 +11,11 @@ const Header = () => {
 
         <nav className="hidden fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent">
           <div className="realative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
-            1 2 3
+            {navigation.map(item => (
+              <a key={item.id} href={item.url}>
+                {item.title}
+              </a>
+            ))}
           </div>
         </nav>
       </div>
