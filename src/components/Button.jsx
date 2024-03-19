4766d92d51b5b4f697e065aa5d62d361e93c3236
henryistyping <mykim1891@gmail.com>
2024-03-18 20:37:18 -0700

    function

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 import ButtonSvg from '../assets/svg/ButtonSvg'
 
 const Button = ({ className, href, onClick, children, px, white, }) => {
-  // The round bracket is asking the function to return the content as a whole.
+  // `functionName = () => ()` is the same as `functionName = () => { return() }`
   // Essentially skipping the statement return(...)
   const classes = `button relative inline-flex items-center justify-center h-11 transition-colors hover:text-color-1 ${px || "px-7"} ${white ? "text-n-8" : "text-n-1"}`;
 
